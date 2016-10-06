9304e210c11b31ca56ad88b6c9148ddd9c4761a1
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-06 17:39:31 +0100

    model object

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,57 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return ['js', 'react', 'html'];
+    return [
+      {
+        "id": 0,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }, {
+        "id":1,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }, {
+        "id": 2,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }, {
+        "id": 3,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }, {
+        "id": 4,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }, {
+        "id": 5,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }, {
+        "id": 6,
+        "category": "Javascript",
+        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "source": "Medium",
+        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "description": "...a good description of differences."
+      }
+    ];
   }
 });
