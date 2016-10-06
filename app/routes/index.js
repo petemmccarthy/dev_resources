83cfffab5e64b7d89f8e45c9731d636de04c55e8
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-06 21:03:49 +0100

    data structure

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
+
     return [
+
       {
         "id": 0,
         "category": "Javascript",
@@ -14,46 +16,111 @@ export default Ember.Route.extend({
       }, {
         "id":1,
         "category": "Javascript",
-        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "title": "Heart and soul of prototypal OO concatenative inheritance - Eric Elliot",
         "source": "Medium",
-        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "href": "https://medium.com/javascript-scene/the-heart-soul-of-prototypal-oo-concatenative-inheritance-a3b64cb27819#.v4uozmef7",
         "description": "...a good description of differences."
       }, {
         "id": 2,
         "category": "Javascript",
-        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "title": "Diff btwn classical and proto inheritance",
         "source": "Medium",
-        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "href": "https://medium.com/javascript-scene/master-the-javascript-interview-what-s-the-difference-between-class-prototypal-inheritance-e4cd0a7562e9#.hhpslkvyp",
         "description": "...a good description of differences."
       }, {
         "id": 3,
         "category": "Javascript",
-        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "title": "For loop vs. array foreach - The JS Guy",
         "source": "Medium",
-        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "href": "http://thejsguy.com/2016/07/30/javascript-for-loop-vs-array-foreach.html",
         "description": "...a good description of differences."
       }, {
         "id": 4,
         "category": "Javascript",
-        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "title": "10 js interview ques",
         "source": "Medium",
-        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "href": "https://medium.com/javascript-scene/10-interview-questions-every-javascript-developer-should-know-6fa6bdf5ad95#.x3b1573q0",
         "description": "...a good description of differences."
       }, {
         "id": 5,
         "category": "Javascript",
-        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "title": "Understanding this once and for all - Quora",
         "source": "Medium",
-        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "href": "https://journeyintojavascript.quora.com/understanding-this-once-and-for-all",
         "description": "...a good description of differences."
       }, {
         "id": 6,
         "category": "Javascript",
-        "title": "Common misconceptions about inheritance in JS - Eric Elliot",
+        "title": "Closures",
         "source": "Medium",
-        "href": "https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.1p2kazift",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 7,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 8,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 9,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 10,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 11,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 12,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 13,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 14,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
+        "description": "...a good description of differences."
+      }, {
+        "id": 15,
+        "category": "Javascript",
+        "title": "Closures",
+        "source": "Medium",
+        "href": "https://spin.atomicobject.com/2014/10/20/javascript-scope-closures/",
         "description": "...a good description of differences."
       }
+
     ];
+
   }
 });
